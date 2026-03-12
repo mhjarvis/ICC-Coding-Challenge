fbe45b75a6a8fb17761e72abb2c939056c18f2c8
Markus Jarvis <markus.h.jarvis@gmail.com>
2026-03-12 10:48:25 -0500

    Add ability to use env variables; setup connection to mongo DB atlas; update variables in evample.env

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
 const app = require("./app")
 
+// Use config.env file
+dotenv.config({ path: "./config.env" })
+
+const DB = process.env.DATABASE.replace(
+	"<db_password>",
+	process.env.DATABASE_PASSWORD,
+)
+
+mongoose.connect(DB).then((con) => {
+	console.log("Connected to Database is successful")
+})
+
 // placeholder - put in .env file
-const port = 8000
+const port = process.env.PORT
 
 // Start server and listen for requests
 app.listen(port, () => {

4f6a5dabd22054b90ae7bdae1d46cbf9179ab53b
Markus Jarvis <markus.h.jarvis@gmail.com>
2026-03-13 13:06:50 -0500

    Update to use config.env variables

--- server.js
+++ server.js
@@ -2,20 +2,22 @@ const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 const app = require("./app")
 
-// Use config.env file
+// Use .env file
 dotenv.config({ path: "./config.env" })
 
+// Asign port from .env file
+const port = process.env.PORT
+
+// Update Database connection string
 const DB = process.env.DATABASE.replace(
 	"<db_password>",
 	process.env.DATABASE_PASSWORD,
 )
 
+// Connect to Database
 mongoose.connect(DB).then((con) => {
 	console.log("Connected to Database is successful")
 })
-
-// placeholder - put in .env file
-const port = process.env.PORT
 
 // Start server and listen for requests
 app.listen(port, () => {

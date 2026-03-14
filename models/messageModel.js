bb2ef6353ffd6ad60be39fc3a45f27032772837f
Markus Jarvis <markus.h.jarvis@gmail.com>
2026-03-14 13:19:35 -0500

    Add additional validation to schema using validator

--- models/messageModel.js
+++ models/messageModel.js
@@ -1,20 +1,27 @@
 const mongoose = require("mongoose")
-
-// Validators
+const validator = require("validator") // Using validator for input validations
 
 // Schema for creating new message document
 const messageSchema = new mongoose.Schema({
 	name: {
 		type: String,
 		required: [true, "Must include a name."],
+		// Validate that there are only letters in the string (ignore spaces)
+		validate: [
+			(str) => validator.isAlpha(str, "en-US", { ignore: " " }),
+			"Name should only include letters.",
+		],
 	},
 	email: {
 		type: String,
 		required: [true, "Must include a email."],
+		// Validate that this is a valid email
+		validate: [validator.isEmail, "Email must be a valid email."],
 	},
 	message: {
 		type: String,
 		required: [true, "Must include a message"],
+		// Validate that messsage is 250 chars or less
 		maxlength: [250, "Message must be 250 characters or less."],
 	},
 	token: {
@@ -22,6 +29,7 @@ const messageSchema = new mongoose.Schema({
 		required: [true, "Token not created"],
 		unique: true,
 	},
+	// Using this bool to control viewing message only once
 	viewed: {
 		type: Boolean,
 		default: false,
@@ -31,9 +39,3 @@ const messageSchema = new mongoose.Schema({
 const Message = mongoose.model("Message", messageSchema)
 
 module.exports = Message
-
-/* Input: JSON, fields:
-- name [String]
-- email [String]
-- message [String]
-Note: all data should be validated reasonably, and the message should be limited to 250 characters */

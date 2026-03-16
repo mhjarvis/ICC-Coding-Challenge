7f4c7ab5b213231d3c3057d0962749569c2f4d72
Markus Jarvis <markus.h.jarvis@gmail.com>
2026-03-16 11:34:44 -0500

    Add test for error formating

--- tests/controller.test.js
+++ tests/controller.test.js
@@ -4,9 +4,29 @@ const { errors, generateToken } = require("../controllers/messageController")
 describe("Token Generation", () => {
 	test("returns token that is a string", () => {
 		const token = generateToken()
-		console.log(token)
+
 		expect(typeof token).toBe("string")
 		expect(token).not.toBe(null)
 		expect(token).not.toBe(undefined)
+	})
+})
+
+// Test errors to return correct error message
+const testError = {
+	errors: {
+		name: {
+			message: "You must include a name",
+		},
+		message: {
+			message: "You must include a message",
+		},
+	},
+}
+
+describe("POST error is formated correctly", () => {
+	test("returns string that summarizes errors", () => {
+		expect(errors(testError)).toBe(
+			"You must include a name; You must include a message",
+		)
 	})
 })
